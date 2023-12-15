ac8e63cfd65460b254eec77b1a08afc0d6d46342
Jenil Patel <94772560+pateljenil139@users.noreply.github.com>
2023-12-15 23:21:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBasgJJQdqBePgdPVdyoZHOnEg0ap47dlM",
-  authDomain: "whatsapp-by-jenilpatel139.firebaseapp.com",
+  apiKey: "//",
+  authDomain: "//",
   projectId: "whatsapp-by-jenilpatel139",
-  storageBucket: "whatsapp-by-jenilpatel139.appspot.com",
-  messagingSenderId: "1013095916026",
-  appId: "1:1013095916026:web:fe23d4c0135da24ae54443",
-  measurementId: "G-PFN2LB1ZH8"
+  storageBucket: "//",
+  messagingSenderId: "//",
+  appId: "//",
+  measurementId: "//"
 };
 
 // Initialize Firebase
